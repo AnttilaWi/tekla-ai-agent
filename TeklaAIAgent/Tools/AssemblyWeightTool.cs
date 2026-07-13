10d0ad8cf8614b42b0927c2674f923c8f643cfa8
anttilawi <anttila.wiljami@gmail.com>
2026-07-13 14:38:39 +0300

    Lisatty tyokalut: kokoonpanojen painot ja palkin luonti
    - AssemblyWeightTool laskee valittujen kokoonpanojen painot (paaosa + sivuosat)
    - CreateBeamTool luo suoran palkin kahden pisteen valiin profiililla ja materiaalilla
    - Tekla Open API -kutsut ajetaan Dispatcher.Invoke-kutsun sisalla saieturvallisuuden vuoksi

---
+++ TeklaAIAgent/Tools/AssemblyWeightTool.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using Microsoft.Extensions.AI;
+using Tekla.Structures.Model;
+using TSMUI = Tekla.Structures.Model.UI;
+
+namespace TeklaAIAgent.Tools;
+
+public class AssemblyWeightTool : ITeklaTool
+{
+    public IEnumerable<AITool> GetFunctions()
+    {
+        yield return AIFunctionFactory.Create(ListSelectedAssemblyWeights);
+    }
+
+    [Description(
+        "Listaa painot kilogrammoina kaikille Tekla Structures -mallista käyttäjän " +
+        "valitsemille kokoonpanoille (assembly). Käytä tätä työkalua, kun käyttäjä " +
+        "kysyy valittujen kappaleiden, osien tai kokoonpanojen painoja.")]
+    private string ListSelectedAssemblyWeights()
+    {
+        return Application.Current.Dispatcher.Invoke(() =>
+        {
+            var model = new Model();
+            if (!model.GetConnectionStatus())
+            {
+                return "Tekla Structuresiin ei saada yhteyttä. Tarkista, että " +
+                       "Tekla Structures on auki ja malli on ladattu.";
+            }
+
+            var selector = new TSMUI.ModelObjectSelector();
+            var selectedObjects = selector.GetSelectedObjects();
+
+            var report = new StringBuilder();
+            int assemblyCount = 0;
+            double totalWeight = 0;
+
+            while (selectedObjects.MoveNext())
+            {
+                if (selectedObjects.Current is Assembly assembly)
+                {
+                    double weight = GetAssemblyWeightKg(assembly);
+                    string label = GetAssemblyLabel(assembly);
+
+                    report.AppendLine($"- {label}: {weight:F1} kg");
+                    totalWeight += weight;
+                    assemblyCount++;
+                }
+            }
+
+            if (assemblyCount == 0)
+            {
+                return "Mallista ei ole valittuna yhtään kokoonpanoa. Valitse " +
+                       "ensin haluamasi kokoonpanot Tekla Structuresin " +
+                       "mallinäkymässä ja pyydä sitten uudelleen.";
+            }
+
+            report.AppendLine();
+            report.AppendLine($"Yhteensä {assemblyCount} kokoonpanoa, " +
+                               $"kokonaispaino {totalWeight:F1} kg.");
+            return report.ToString();
+        });
+    }
+
+    private static double GetAssemblyWeightKg(Assembly assembly)
+    {
+        double totalWeight = 0;
+
+        if (assembly.GetMainPart() is ModelObject mainPart)
+        {
+            double mainWeight = 0;
+            mainPart.GetReportProperty("WEIGHT", ref mainWeight);
+            totalWeight += mainWeight;
+        }
+
+        foreach (var secondaryObj in assembly.GetSecondaries())
+        {
+            if (secondaryObj is ModelObject secondaryPart)
+            {
+                double secondaryWeight = 0;
+                secondaryPart.GetReportProperty("WEIGHT", ref secondaryWeight);
+                totalWeight += secondaryWeight;
+            }
+        }
+
+        return totalWeight;
+    }
+
+    private static string GetAssemblyLabel(Assembly assembly)
+    {
+        if (assembly.GetMainPart() is Part mainPart && !string.IsNullOrWhiteSpace(mainPart.Name))
+        {
+            return $"{mainPart.Name} (assembly #{assembly.Identifier.ID})";
+        }
+
+        return $"Assembly #{assembly.Identifier.ID}";
+    }
+}
---
+++ TeklaAIAgent/Tools/CreateBeamTool.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using Microsoft.Extensions.AI;
+using Tekla.Structures.Model;
+using TSGeo = Tekla.Structures.Geometry3d;
+
+namespace TeklaAIAgent.Tools;
+
+public class CreateBeamTool : ITeklaTool
+{
+    public IEnumerable<AITool> GetFunctions()
+    {
+        yield return AIFunctionFactory.Create(CreateBeam);
+    }
+
+    [Description(
+        "Luo uuden suoran palkin Tekla Structures -malliin kahden pisteen välille. " +
+        "Koordinaatit ovat millimetrejä mallin globaalissa koordinaatistossa.")]
+    private string CreateBeam(
+        [Description("Alkupisteen X-koordinaatti (mm).")] double startX,
+        [Description("Alkupisteen Y-koordinaatti (mm).")] double startY,
+        [Description("Alkupisteen Z-koordinaatti (mm).")] double startZ,
+        [Description("Loppupisteen X-koordinaatti (mm).")] double endX,
+        [Description("Loppupisteen Y-koordinaatti (mm).")] double endY,
+        [Description("Loppupisteen Z-koordinaatti (mm).")] double endZ,
+        [Description(
+            "Tekla-profiilimerkintä, esim. 'HEA200' tai '200*200'. Käytä " +
+            "'HEA200', jos käyttäjä ei anna profiilia.")]
+        string profile = "HEA200",
+        [Description(
+            "Tekla-materiaalimerkintä, esim. 'S355J2' teräkselle tai 'C30/37' " +
+            "betonille. Käytä 'S355J2', jos käyttäjä ei anna materiaalia.")]
+        string material = "S355J2")
+    {
+        return Application.Current.Dispatcher.Invoke(() =>
+        {
+            var model = new Model();
+            if (!model.GetConnectionStatus())
+            {
+                return "Tekla Structuresiin ei saada yhteyttä. Tarkista, että " +
+                       "Tekla Structures on auki ja malli on ladattu.";
+            }
+
+            var beam = new Beam
+            {
+                StartPoint = new TSGeo.Point(startX, startY, startZ),
+                EndPoint = new TSGeo.Point(endX, endY, endZ)
+            };
+            beam.Profile.ProfileString = profile;
+            beam.Material.MaterialString = material;
+
+            bool inserted = beam.Insert();
+            if (!inserted)
+            {
+                return "Palkin luonti epäonnistui. Tarkista profiilin ja " +
+                       "materiaalin merkinnät sekä pisteiden koordinaatit.";
+            }
+
+            model.CommitChanges();
+
+            return $"Palkki luotu onnistuneesti. Alkupiste ({startX}, {startY}, " +
+                   $"{startZ}), loppupiste ({endX}, {endY}, {endZ}), profiili " +
+                   $"{profile}, materiaali {material}. Tekla-tunniste: " +
+                   $"{beam.Identifier.ID}.";
+        });
+    }
+}
